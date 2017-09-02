2885e4cf9b84312893ea9d5d786e9f110084f95f
Ajay Kumar <ajay.expert@live.com>
2017-09-03 00:23:29 +0530

    check 2

--- resume/assets/styleswitcher/styleswitcher.js
+++ resume/assets/styleswitcher/styleswitcher.js
@@ -1,7 +1,63 @@
 $(document).ready(function() {
+	var html = `<div id="style-switcher">
+		<html>
+			<head>
+				<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+			</head>
+			<body>
+				<span class="ss-toggle bg-primary">
+				<i class="fa fa-gear fa-spin"></i></span>
+				<span class="ss-label">Color scheme:</span>
+				<ul id="color-switcher">
+				    <li>
+					    <a href="assets/styleswitcher/styleswitcher.html#" data-scheme="blue-beige">
+					    	<span class="bg-blue"></span>
+					    	<span class="bg-blue-navy"></span>
+					    	<span class="bg-beige"></span>
+					    </a>
+				    </li>
+				    <li>
+					    <a href="assets/styleswitcher/styleswitcher.html#" data-scheme="red-blue">
+					    	<span class="bg-red"></span>
+					    	<span class="bg-red-dark"></span>
+					    	<span class="bg-blue"></span>
+					    </a>
+				    </li>
+				    <li>
+					    <a href="assets/styleswitcher/styleswitcher.html#" data-scheme="orange-green">
+					    	<span class="bg-orange"></span>
+					    	<span class="bg-brown"></span>
+					    	<span class="bg-green"></span>
+					    </a>
+				    </li>
+				    <li>
+					    <a href="assets/styleswitcher/styleswitcher.html#" data-scheme="green-orange">
+					    	<span class="bg-green"></span>
+					    	<span class="bg-green-dark"></span>
+					    	<span class="bg-orange"></span>
+					    </a>
+				    </li>
+				    <li>
+					    <a href="assets/styleswitcher/styleswitcher.html#" data-scheme="purple-yellow">
+					    	<span class="bg-purple"></span>
+					    	<span class="bg-purple-navy"></span>
+					    	<span class="bg-yellow"></span>
+					    </a>
+				    </li>
+				    <li>
+					    <a href="assets/styleswitcher/styleswitcher.html#" data-scheme="brown-beige">
+					    	<span class="bg-beige"></span>
+					    	<span class="bg-brown"></span>
+					    	<span class="bg-beige"></span>
+					    </a>
+				    </li>
+				</ul>
+			</body>
+		</html>
+	</div>`;
 
-	$("body").append('<div id="style-switcher"></div>');
-	$("#style-switcher").load( "assets/styleswitcher/styleswitcher.html" );
+	$("body").append(html);
+	// $("#style-switcher").load( "assets/styleswitcher/styleswitcher.html" );
 
 	setTimeout(function() {
 
@@ -27,7 +83,6 @@ $(document).ready(function() {
 		})
 	
 		$('#style-switcher .ss-toggle').click(function(){
-			alert('working');
 			var div = $('#style-switcher');
 			if (div.css('right') === '-175px') {
 				$('#style-switcher').animate({
